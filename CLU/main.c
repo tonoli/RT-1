51d380ef69b0f1f76a85a807242a8e135cae5d10
Nicolas Sampré <nsampre@student.42.fr>
2017-11-29 16:40:32 +0100

    Initial Commit

---
+++ CLU/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CLU
+//
+//  Created by Nicolas SAMPRE on 11/29/17.
+//  Copyright © 2017 Nicolas SAMPRE. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
