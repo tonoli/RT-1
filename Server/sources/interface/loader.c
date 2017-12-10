fa3b70f42500065063d94a9d23523230541bf866
Ignacio Tonoli <itonoli-@student.42.fr>
2017-12-10 21:46:46 +0100

    Et tu normes normes normes c'est ta façon d'aimer

--- Server/sources/interface/loader.c
+++ Server/sources/interface/loader.c
@@ -6,11 +6,36 @@
 /*   By: itonoli- <itonoli-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/04 22:49:35 by itonoli-          #+#    #+#             */
-/*   Updated: 2017/12/10 02:21:18 by itonoli-         ###   ########.fr       */
+/*   Updated: 2017/12/10 21:40:15 by itonoli-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
+
+void		cli_arrived(t_env *e)
+{
+	create_rect(e->s_background,
+				(SDL_Rect){490, 670 - 30, 500, 30}, 0x242424);
+	if (e->nb_cli == 1)
+		sprintf(e->txt[22],
+			"%d CLIENT CONNECTED TO THE SERVER", e->nb_cli);
+	else
+		sprintf(e->txt[22],
+			"%d CLIENTS CONNECTED TO THE SERVER", e->nb_cli);
+	e->s_tmp = TTF_RenderText_Shaded(e->font[0], e->txt[22],
+			COLOR_W, COLOR_LO);
+	SDL_BlitSurface(e->s_tmp, &(SDL_Rect){0, 0, e->s_tmp->w,
+		e->s_tmp->h}, e->s_background, &(SDL_Rect){490, 670 - 30,
+			e->s_tmp->w, e->s_tmp->h});
+	SDL_FreeSurface(e->s_tmp);
+	e->s_tmp = TTF_RenderText_Shaded(e->font[2],
+			"    YOU CAN PRESS ANY KEY TO START...    ",
+			COLOR_Y, COLOR_LO);
+	SDL_BlitSurface(e->s_tmp, &(SDL_Rect){0, 0, e->s_tmp->w,
+		e->s_tmp->h}, e->s_background, &(SDL_Rect){540, 704 - 30,
+			e->s_tmp->w, e->s_tmp->h});
+	SDL_FreeSurface(e->s_tmp);
+}
 
 void		loader_wait(t_env *e)
 {
@@ -31,28 +56,6 @@ void		loader_wait(t_env *e)
 			SDL_FreeSurface(e->s_tmp);
 		}
 		else if (e->nb_cli > 0)
-		{
-			create_rect(e->s_background,
-						(SDL_Rect){490, 670 - 30, 500, 30}, 0x242424);
-			if (e->nb_cli == 1)
-				sprintf(e->txt[22],
-					"%d CLIENT CONNECTED TO THE SERVER", e->nb_cli);
-			else
-				sprintf(e->txt[22],
-					"%d CLIENTS CONNECTED TO THE SERVER", e->nb_cli);
-			e->s_tmp = TTF_RenderText_Shaded(e->font[0], e->txt[22],
-					COLOR_W, COLOR_LO);
-			SDL_BlitSurface(e->s_tmp, &(SDL_Rect){0, 0, e->s_tmp->w,
-				e->s_tmp->h}, e->s_background, &(SDL_Rect){490, 670 - 30,
-					e->s_tmp->w, e->s_tmp->h});
-			SDL_FreeSurface(e->s_tmp);
-			e->s_tmp = TTF_RenderText_Shaded(e->font[2],
-					"    YOU CAN PRESS ANY KEY TO START...    ",
-					COLOR_Y, COLOR_LO);
-			SDL_BlitSurface(e->s_tmp, &(SDL_Rect){0, 0, e->s_tmp->w,
-				e->s_tmp->h}, e->s_background, &(SDL_Rect){540, 704 - 30,
-					e->s_tmp->w, e->s_tmp->h});
-			SDL_FreeSurface(e->s_tmp);
-		}
+			cli_arrived(e);
 	}
 }
